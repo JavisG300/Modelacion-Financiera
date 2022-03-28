6b6c1b7dfb966f8170b6bd7481fc09e23574b174
Javier Andrade <javieregonzaleza@gmail.com>
2022-03-28 16:00:19 -0600

    Ejemplo petroleo

--- arbitraje.py
+++ arbitraje.py
@@ -7,10 +7,10 @@ def main(S0, K, T, TLR):
     else:
         print(F,'<',K,'\n','Se toma una posicion larga respecto al activo (se compra con dinero del banco) en: ' + str(S0))
     
-S0  = 21    #Precio hoy del activo subyacente
-K   = 24    #Precio de ejercicio del futuro o forward
-T   = 12/12    #Fecha caducación en meses
-TLR = 0.05    #Tasa libre de riesgo
+S0  = 64.40    #Precio hoy del activo subyacente
+K   = 71.20    #Precio de ejercicio del futuro o forward
+T   = 9/12    #Fecha caducación en meses
+TLR = 0.10    #Tasa libre de riesgo
 
 if __name__ == '__main__':
     main(S0, K, T, TLR)
