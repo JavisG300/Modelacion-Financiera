ad24d606bd4d84a776de20602c05c4764012c294
Javier Andrade <javieregonzaleza@gmail.com>
2022-03-28 15:48:45 -0600

    Ejemplo gasolina

--- arbitraje.py
+++ arbitraje.py
@@ -7,10 +7,10 @@ def main(S0, K, T, TLR):
     else:
         print(F,'<',K,'Se toma una posicion larga respecto al activo (se compra con dinero del banco)')
     
-S0  = 1.05    #Precio hoy del activo subyacente
-K   = 1.05    #Precio de ejercicio del futuro o forward
-T   = 2/12    #Fecha caducación en meses
-TLR = 0.02    #Tasa libre de riesgo
+S0  = 21    #Precio hoy del activo subyacente
+K   = 24    #Precio de ejercicio del futuro o forward
+T   = 12/12    #Fecha caducación en meses
+TLR = 0.05    #Tasa libre de riesgo
 
 if __name__ == '__main__':
     main(S0, K, T, TLR)
