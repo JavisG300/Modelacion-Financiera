ff1168bb07dd86553c683dd221d33acf4ab7e949
Javier Andrade <javieregonzaleza@gmail.com>
2022-03-28 15:42:54 -0600

    Arbitraje 2.0

--- arbitraje.py
+++ arbitraje.py
@@ -1,17 +1,16 @@
 import numpy as np
-def main():
-    S0  = 1.05    #Precio hoy del activo subyacente
-    K   = 1.05    #Precio de ejercicio del futuro o forward
-    T   = 2/12    #Fecha caducación en meses
-    TLR = 0.02    #Tasa libre de riesgo
+def main(S0, K, T, TLR):
     F = S0*np.exp(TLR*T)
     
     if F > K:
-        print('Se toma una posición corta respecto al activo (se vende)')
+        print(F,'>',K,'Se toma una posición corta respecto al activo (se vende)')
     else:
-        print('Se toma una posicion larga respecto al activo (se compra)')
+        print(F,'<',K,'Se toma una posicion larga respecto al activo (se compra con dinero del banco)')
     
-
+S0  = 1.05    #Precio hoy del activo subyacente
+K   = 1.05    #Precio de ejercicio del futuro o forward
+T   = 2/12    #Fecha caducación en meses
+TLR = 0.02    #Tasa libre de riesgo
 
 if __name__ == '__main__':
-    main()
+    main(S0, K, T, TLR)
