4a08fdb06893671e980d2d29a34a5f5aa348e953
Javier Andrade <javieregonzaleza@gmail.com>
2022-05-02 17:11:58 -0500

    Declaración de variables Modelo Binomial

--- ModeloBinomial.py
+++ ModeloBinomial.py
@@ -1,7 +1,7 @@
 def mbinomial():
     pass
 
-s      = int(input("Indica el precio incial del activo subyacente"))
+s      = int(input("Indica el precio incial del activo subyacente: "))
 opcion = int(input("""
 Escribe el número de la opcion que será valuada
 1) Call Europeo
@@ -9,6 +9,12 @@ Escribe el número de la opcion que será valuada
 3) Call Americano
 4) Put Americano
 """)) 
+T      = int(input("Indica el tiempo de vencimiento: "))
+n      = int(input("Indica el número de periodos: "))
+r      = int(input("Indica la tasa de interés anualizada: "))
+k      = int(input("Indica el precio de ejercicio: "))
+u      = int(input("Indica el porcentaje de subida: 1 + 0."))
+d      = int(input("Indica el porcentaje de bajada: 1 - 0."))
 
 if __name__ == '__main__':
     mbinomial()
