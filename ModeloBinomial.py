0b5f8a0fcb2f92a0976fadca824f0c5c325f8989
Javier Andrade <javieregonzaleza@gmail.com>
2022-05-02 17:04:46 -0500

    Inicio Modelo Binomial

---
+++ ModeloBinomial.py
@@ -0,0 +1,14 @@
+def mbinomial():
+    pass
+
+s      = int(input("Indica el precio incial del activo subyacente"))
+opcion = int(input("""
+Escribe el número de la opcion que será valuada
+1) Call Europeo
+2) Put Europeo
+3) Call Americano
+4) Put Americano
+""")) 
+
+if __name__ == '__main__':
+    mbinomial()
