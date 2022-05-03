bc752b02c4a898ef1ce111900e664dd1f02b4aa4
Javier Andrade <javieregonzaleza@gmail.com>
2022-05-02 19:26:41 -0500

    Declaración de variables Modelo Binomial e inicio de la funcion ya imprime una lista con los valores up y down

--- ModeloBinomial.py
+++ ModeloBinomial.py
@@ -1,30 +1,61 @@
 import string
+from urllib.request import ProxyBasicAuthHandler
+import numpy as np
+from pandas import DataFrame #Para hacer una tabla con los datos
 
 def mbinomial(s,opcion,T,n,r,k,u,d):
-    abecedario = list(string.ascii_uppercase)
-    nodos      = {"A":s}
-    lista_nodos=nodos.values()
-    for i in range(n): #Por cada piso añadir y restar ¿como?
-        nodos[abecedario[i+1]] = s*(1+u)
-        nodos[abecedario[i+2]] = s*(1-d)
-        lista_nodos.append(nodos.values())
-    print(nodos)
+    Dt = T/n
+    Propabilidad = (np.exp(r*Dt) - d)/(u - d)
+    uno_probabilidad = 1 - Propabilidad
+    valor_presente1 = np.exp(-r*Dt)
+    valor_presenteT = np.exp(-r*T)
+ #   S = np.zeros((n+1,n+1))
+ #   S[0,0]=s
+
+ #   for i in range(1,n+1):
+ #       S[0,i] = S[0,i-1]*u
+ #       for j in range(1,n+1):
+ #           S[j,i]=S[j-1,i-1]*d
+ #   VOPC = np.zeros((n+1,n+1))
+
+ #   for i in range(n+1):
+ #       VOPC[i,n]=max(cop*(S[i,n]-k),0)
+
+    lista_nodos = [s]
+    if opcion == 1:
+        for i in range(1,n+2):
+            up = s*u
+            lista_nodos.append(up)
+            down = s*d
+            lista_nodos.append(down)
+
+            s = lista_nodos[i]
+            
+        print(lista_nodos, Propabilidad)
+    elif opcion == 2:
+        pass
+    elif opcion == 3:
+        pass
+    else:
+        pass
+
+
 
 
 s      = float(input("Indica el precio incial del activo subyacente: "))
 opcion = float(input("""
-Escribe el número de la opcion4 que será valuada
+Escribe el número de la opción que será valuada
 1) Call Europeo
 2) Put Europeo
 3) Call Americano
 4) Put Americano
 """)) 
-T      = float(input("Indica el tiempo de vencimiento: "))
-n      = float(input("Indica el número de periodos: "))
+T      = float(input("Indica el tiempo de vencimiento en años (n/12): "))
+n      = int(input("Indica el número de periodos: "))
 r      = float(input("Indica la tasa de interés anualizada: "))
 k      = float(input("Indica el precio de ejercicio: "))
-u      = float(input("Indica el porcentaje de subida: 1 + 0."))
-d      = float(input("Indica el porcentaje de bajada: 1 - 0."))
+u      = float(input("Indica el porcentaje/probabilidad de subida con la unidad: "))
+d      = float(input("Indica el porcentaje/probabilidad de bajada con la unidad: "))
 
 if __name__ == '__main__':
-    mbinomial()
+    mbinomial(s,opcion,T,n,r,k,u,d)
