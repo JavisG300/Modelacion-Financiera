dcb8a6050677734409bf9971637bc18e52c01bf6
Javier Andrade <javieregonzaleza@gmail.com>
2022-05-05 13:06:39 -0500

    Calculadora Modelo Binomial v2.0

--- ModeloBinomial.py
+++ ModeloBinomial.py
@@ -72,7 +72,6 @@ def mbinomial(s,opcion,T,n,r,k,u,d): #Funcion para determinar el precio de la op
         Nodos = nodos(s,u,d,n)
         lista_de_nodos = []
         lcu_y_cd = []
-        print('\n','El Call Europeo tiene los siguientes nodos: \n', Nodos)
         for value in Nodos.values():
             valor = value 
             lista_de_nodos.append(valor)
@@ -84,34 +83,31 @@ def mbinomial(s,opcion,T,n,r,k,u,d): #Funcion para determinar el precio de la op
             combinancion = factorial(n)/((factorial(i))*(factorial(n-i)))
             cuenta = cuenta + combinancion*(Propabilidad**(n-i))*((uno_probabilidad)**i)*cu 
         valor_del_call = valor_presente1 * cuenta
-        print("El valor del Call Europeo es: ", valor_del_call)
+        return valor_del_call, Nodos
 
     elif opcion == 2:
         cuenta = 0
         Nodos = nodos(s,u,d,n)
         lista_de_nodos = []
-        lcu_y_cd = []
-        print('\n','El Put Europeo tiene los siguientes nodos: \n', Nodos)        
+        lcu_y_cd = []     
         for value in Nodos.values():
             valor = value 
             lista_de_nodos.append(valor)
         for i in range(len(lista_de_nodos)-1,len(lista_de_nodos)-(n+2),-1):
             lcu_y_cd.append(lista_de_nodos[i])
         lcu_y_cd = lcu_y_cd[::-1]
-        print(lcu_y_cd)
         for i in range(n+1):
             cu = max(k-lcu_y_cd[i],0)
             combinancion = factorial(n)/((factorial(i))*(factorial(n-i)))
             cuenta = cuenta + combinancion*(Propabilidad**(n-i))*((uno_probabilidad)**i)*cu 
         valor_del_put = valor_presente1 * cuenta
-        print("El valor del Put Europeo es: ", valor_del_put)
+        return valor_del_put, Nodos
 
     elif opcion == 3:
         Nodos = nodos(s,u,d,n)
         lista_de_nodos = []
         lcu_y_cd = []
         lista_llaves = []
-        print('\n','El Call Europeo tiene los siguientes nodos: \n', Nodos)
         for value in Nodos.values():
             valor = value 
             lista_de_nodos.append(valor)
@@ -138,8 +134,7 @@ def mbinomial(s,opcion,T,n,r,k,u,d): #Funcion para determinar el precio de la op
             Nodos[lista_llaves[i]] = lcu_y_cd[i]
         valor_del_call_americano = valor_presente1*(Propabilidad*lcu_y_cd[1] + uno_probabilidad*lcu_y_cd[2])
         Nodos['A'] = valor_del_call_americano
-        print(f'El valor del Call Americano es: {valor_del_call_americano}')
-        print('Los nodos del Call Americano son: \n', Nodos)
+        return valor_del_call_americano, Nodos
 
 
     elif opcion == 4:
@@ -147,7 +142,6 @@ def mbinomial(s,opcion,T,n,r,k,u,d): #Funcion para determinar el precio de la op
         lista_de_nodos = []
         lcu_y_cd = []
         lista_llaves = []
-        print('\n','El Put Europeo tiene los siguientes nodos: \n', Nodos)
         for value in Nodos.values():
             valor = value 
             lista_de_nodos.append(valor)
@@ -174,28 +168,65 @@ def mbinomial(s,opcion,T,n,r,k,u,d): #Funcion para determinar el precio de la op
             Nodos[lista_llaves[i]] = lcu_y_cd[i]
         valor_del_put_americano = valor_presente1*(Propabilidad*lcu_y_cd[1] + uno_probabilidad*lcu_y_cd[2])
         Nodos['A'] = valor_del_put_americano
-        print(f'El valor del Put Americano es: {valor_del_put_americano}')
-        print('Los nodos del Put Americano son: \n', Nodos)
+        return valor_del_put_americano, Nodos
 
+def tabla_comparativa(s,T,n,t,k,u,d):
+    datos={'Opción':['Europeo', 'Europeo','Americano', 'Americano'], 'Precio/Nodos':[mbinomial(s,1,T,n,r,k,u,d),mbinomial(s,2,T,n,r,k,u,d),mbinomial(s,3,T,n,r,k,u,d),mbinomial(s,4,T,n,r,k,u,d)]}
+    tabla_datos = DataFrame(datos, columns = ['Opción','Precio/Nodos'], 
+    index=['Call','Put','Call','Put'])
+    print('\n')
+    print(tabla_datos.round(4))
 
+print("""
+--------------------------------------------------------------------
+|| Bienvenido a la calculadora de opciones con el modelo binomial || 
+||                                                                ||
+|| Por favor introduce los datos que se solicitan a continuación  ||
+--------------------------------------------------------------------
 
-print(""" Este programa calcula el modelo binomial para opciones financieras \n """)
-s      = float(input("Indica el precio incial del activo subyacente: "))
+""")
 opcion = float(input("""
 Escribe el número de la opción que será valuada
 1) Call Europeo
 2) Put Europeo
 3) Call Americano
-4) Put Americano
-""")) 
+4) Put Americano 
+ """)) 
+print(f'---Su selección fue el inciso {opcion} ---\n')
+s      = float(input("Indica el precio incial del activo subyacente: "))
 T      = float(input("Indica el tiempo de vencimiento en meses: "))
 n      = int(input("Indica el número de periodos: "))
-r      = float(input("Indica la tasa de interés anualizada: "))
+r      = float(input("Indica la tasa de interés anualizada de forma decimal: "))
 k      = float(input("Indica el precio de ejercicio: "))
 u      = float(input("Indica el porcentaje/probabilidad de subida con la unidad: "))
 d      = float(input("Indica el porcentaje/probabilidad de bajada con la unidad: "))
 
-if __name__ == '__main__':
+def main():
+    print("""
+    ---------------------------------------
+    || Resumen de los datos introducidos ||
+    ---------------------------------------
+     """)
     tabla(s,opcion,T,n,r,k,u,d)
     nodos(s,u,d,n)
-    mbinomial(s,opcion,T,n,r,k,u,d)
+    l = mbinomial(s,opcion,T,n,r,k,u,d)
+    if opcion == 1:
+        tipo = 'Call Europeo'
+    elif opcion == 2:
+        tipo = 'Put Europeo'
+    elif opcion ==3:
+        tipo = 'Call Americano'
+    elif opcion ==4:
+        tipo = 'Put Americano'
+    print('\n')
+    print(f'El precio del {tipo} solicitado es:{l[0]} \n')
+    print(f'Los nodos de la ocpión son: {l[1]} \n \n')
+    print("""
+    ---------------------------------------------------------------------------------------------------------------
+    || A continuación se muestra una tabla comparativa de la opción solicitada junto a los demás tipos de opción || 
+    ---------------------------------------------------------------------------------------------------------------
+    """)
+    tabla_comparativa(s,T,n,r,k,u,d)
+
+if __name__ == '__main__':
+    main()
