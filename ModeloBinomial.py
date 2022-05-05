a0d0943499a1abdc634e079274aeb7f1a4f18a2e
Javier Andrade <javieregonzaleza@gmail.com>
2022-05-05 12:05:43 -0500

    Todas las opciones correctas v1.0

--- ModeloBinomial.py
+++ ModeloBinomial.py
@@ -70,12 +70,12 @@ def mbinomial(s,opcion,T,n,r,k,u,d): #Funcion para determinar el precio de la op
     if opcion == 1:
         cuenta = 0
         Nodos = nodos(s,u,d,n)
-        print('\n','El Call Europeo tiene los siguientes nodos: \n', Nodos)
         lista_de_nodos = []
+        lcu_y_cd = []
+        print('\n','El Call Europeo tiene los siguientes nodos: \n', Nodos)
         for value in Nodos.values():
             valor = value 
             lista_de_nodos.append(valor)
-        lcu_y_cd = []
         for i in range(len(lista_de_nodos)-1,len(lista_de_nodos)-(n+2),-1):
             lcu_y_cd.append(lista_de_nodos[i])
         lcu_y_cd = lcu_y_cd[::-1]
@@ -89,12 +89,12 @@ def mbinomial(s,opcion,T,n,r,k,u,d): #Funcion para determinar el precio de la op
     elif opcion == 2:
         cuenta = 0
         Nodos = nodos(s,u,d,n)
-        print('\n','El Put Europeo tiene los siguientes nodos: \n', Nodos)
         lista_de_nodos = []
+        lcu_y_cd = []
+        print('\n','El Put Europeo tiene los siguientes nodos: \n', Nodos)        
         for value in Nodos.values():
             valor = value 
             lista_de_nodos.append(valor)
-        lcu_y_cd = []
         for i in range(len(lista_de_nodos)-1,len(lista_de_nodos)-(n+2),-1):
             lcu_y_cd.append(lista_de_nodos[i])
         lcu_y_cd = lcu_y_cd[::-1]
@@ -106,38 +106,76 @@ def mbinomial(s,opcion,T,n,r,k,u,d): #Funcion para determinar el precio de la op
         valor_del_put = valor_presente1 * cuenta
         print("El valor del Put Europeo es: ", valor_del_put)
 
-    elif opcion == 3: #ESTA MAL TIENES QUE TOMAR EN CUENTA LOS ULTIMOS PUU Y PDD
+    elif opcion == 3:
         Nodos = nodos(s,u,d,n)
-        print('\n','El Call Europeo tiene los siguientes nodos: \n', Nodos)
         lista_de_nodos = []
+        lcu_y_cd = []
+        lista_llaves = []
+        print('\n','El Call Europeo tiene los siguientes nodos: \n', Nodos)
         for value in Nodos.values():
             valor = value 
             lista_de_nodos.append(valor)
-        lcu_y_cd = []
         for i in range(len(lista_de_nodos)-(n+1)):
             lcu_y_cd.append(lista_de_nodos[i])
         lcu_y_cd = lcu_y_cd[::-1]
-        print(lcu_y_cd)
         for i in range(len(lcu_y_cd)-1):
             nodo_evaluar = lcu_y_cd[i]
             up = nodo_evaluar*u
+            maxup = max(k-up,0)
             down = nodo_evaluar*d
-            call = valor_presente1*(up*Propabilidad + down*uno_probabilidad)
+            maxdown = max(k-down,0)
+            call = valor_presente1*(maxup*Propabilidad + maxdown*uno_probabilidad)
             rendimiento_ejercer = max(k-nodo_evaluar,0)
             if rendimiento_ejercer > call:
                 lcu_y_cd[i] = rendimiento_ejercer
             else:
-                lcu_y_cd[i] = lcu_y_cd[i]
+                lcu_y_cd[i] = call
         lcu_y_cd = lcu_y_cd[::-1]
-        Nodos['A']=lcu_y_cd[0]
-        Nodos['B']=lcu_y_cd[1]
-        Nodos['C']=lcu_y_cd[2]
-        valor_del_call_americano = valor_presente1*(Propabilidad*lcu_y_cd[1] + uno_probabilidad*lcu_y_cd[0])
-        print('El valor del Call Americano es: ', valor_del_call_americano)
+        for value in Nodos.keys():
+            llave = value 
+            lista_llaves.append(llave)
+        for i in range(len(lcu_y_cd)):
+            Nodos[lista_llaves[i]] = lcu_y_cd[i]
+        valor_del_call_americano = valor_presente1*(Propabilidad*lcu_y_cd[1] + uno_probabilidad*lcu_y_cd[2])
+        Nodos['A'] = valor_del_call_americano
+        print(f'El valor del Call Americano es: {valor_del_call_americano}')
+        print('Los nodos del Call Americano son: \n', Nodos)
 
 
     elif opcion == 4:
-        pass
+        Nodos = nodos(s,u,d,n)
+        lista_de_nodos = []
+        lcu_y_cd = []
+        lista_llaves = []
+        print('\n','El Put Europeo tiene los siguientes nodos: \n', Nodos)
+        for value in Nodos.values():
+            valor = value 
+            lista_de_nodos.append(valor)
+        for i in range(len(lista_de_nodos)-(n+1)):
+            lcu_y_cd.append(lista_de_nodos[i])
+        lcu_y_cd = lcu_y_cd[::-1]
+        for i in range(len(lcu_y_cd)-1):
+            nodo_evaluar = lcu_y_cd[i]
+            up = nodo_evaluar*u
+            maxup = max(k-up,0)
+            down = nodo_evaluar*d
+            maxdown = max(k-down,0)
+            put = valor_presente1*(maxup*Propabilidad + maxdown*uno_probabilidad)
+            rendimiento_ejercer = max(k-nodo_evaluar,0)
+            if rendimiento_ejercer > put:
+                lcu_y_cd[i] = rendimiento_ejercer
+            else:
+                lcu_y_cd[i] = put
+        lcu_y_cd = lcu_y_cd[::-1]
+        for value in Nodos.keys():
+            llave = value 
+            lista_llaves.append(llave)
+        for i in range(len(lcu_y_cd)):
+            Nodos[lista_llaves[i]] = lcu_y_cd[i]
+        valor_del_put_americano = valor_presente1*(Propabilidad*lcu_y_cd[1] + uno_probabilidad*lcu_y_cd[2])
+        Nodos['A'] = valor_del_put_americano
+        print(f'El valor del Put Americano es: {valor_del_put_americano}')
+        print('Los nodos del Put Americano son: \n', Nodos)
 
 
 
