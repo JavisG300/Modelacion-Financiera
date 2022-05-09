443fad65657283caf1e3440042c4ef7dc7ad18c0
Javier Andrade <javieregonzaleza@gmail.com>
2022-05-09 18:50:52 -0500

    comments 3.1

--- ModeloBinomial.py
+++ ModeloBinomial.py
@@ -5,7 +5,7 @@ from math import factorial
 from paridad_put_call import paridad #Función de paridad put call programada anteriormente
 
 def nodos(s,u,d,n):
-    abecedario = list(string.ascii_uppercase)  #Para hacer lista de nodos con letra
+    abecedario = list(string.ascii_uppercase)  #Lista de abecedario (25 letras)
     numeros = [numero+1 for numero in range(1,n**n)]
     dic_nodos = {}
     lista_nodos = [s]
