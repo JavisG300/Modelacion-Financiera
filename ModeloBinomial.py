8d40981b814f4bc52bb94e8e80260435ccaf528c
Javier Andrade <javieregonzaleza@gmail.com>
2022-05-10 01:05:00 -0500

    Some changes, deleting dict

--- ModeloBinomial.py
+++ ModeloBinomial.py
@@ -1,18 +1,16 @@
-import string                #Para hacer el diccionario de nodos con letras
+from operator import index
 import numpy as np           #Para exp
 from pandas import DataFrame #Para hacer una tabla con los datos
 from math import factorial
 from paridad_put_call import paridad #Función de paridad put call programada anteriormente
 
 def nodos(s,u,d,n):
-    abecedario = list(string.ascii_uppercase)  #Lista de abecedario (25 letras)
     numeros = [numero+1 for numero in range(1,n**n)]
-    dic_nodos = {}
+    ramas = [numeros[rama] for rama in range(n)]
+    longitud = sum(ramas)
     lista_nodos = [s]
     j = 0
     i = 0
-    ramas = [numeros[rama] for rama in range(n)]
-    longitud = sum(ramas)
     while len(lista_nodos) <= longitud:
         up = s*u
         lista_nodos.append(up)
@@ -34,10 +32,7 @@ def nodos(s,u,d,n):
             lista_nodos = lista_nodos
         s = lista_nodos[i+1]
         i+=1
-    for i in lista_nodos:
-        dic_nodos[abecedario[j]] = lista_nodos[j] 
-        j += 1
-    return dic_nodos
+    return lista_nodos
 
 def tabla(s,opcion,T,n,r,k,u,d):
     T = T/12
@@ -72,86 +67,86 @@ def mbinomial(s,opcion,T,n,r,k,u,d): #Funcion para determinar el precio de la op
     if opcion == 1:   #Call Europeo
         cuenta = 0
         Nodos = nodos(s,u,d,n)
-        lista_de_nodos = []
         lcu_y_cd = []
-        for value in Nodos.values():
-            valor = value 
-            lista_de_nodos.append(valor)
-        for i in range(len(lista_de_nodos)-1,len(lista_de_nodos)-(n+2),-1):
-            lcu_y_cd.append(lista_de_nodos[i])
+        for i in range(len(Nodos)-1,len(Nodos)-(n+2),-1):
+            lcu_y_cd.append(Nodos[i])
         lcu_y_cd = lcu_y_cd[::-1]
         for i in range(n+1):
             cu = max(lcu_y_cd[i]-k,0)
-            combinancion = factorial(n)/((factorial(i))*(factorial(n-i)))
-            cuenta = cuenta + combinancion*(Propabilidad**(n-i))*((uno_probabilidad)**i)*cu 
+            combinacion = factorial(n)/((factorial(i))*(factorial(n-i)))
+            cuenta = cuenta + combinacion*(Propabilidad**(n-i))*((uno_probabilidad)**i)*cu 
         valor_del_call = valor_presente1 * cuenta
         return valor_del_call, Nodos
 
     elif opcion == 2:  #Put Europeo
         cuenta = 0
         Nodos = nodos(s,u,d,n)
-        lista_de_nodos = []
         lcu_y_cd = []     
-        for value in Nodos.values():
-            valor = value 
-            lista_de_nodos.append(valor)
-        for i in range(len(lista_de_nodos)-1,len(lista_de_nodos)-(n+2),-1):
-            lcu_y_cd.append(lista_de_nodos[i])
+        for i in range(len(Nodos)-1,len(Nodos)-(n+2),-1):
+            lcu_y_cd.append(Nodos[i])
         lcu_y_cd = lcu_y_cd[::-1]
         for i in range(n+1):
             cu = max(k-lcu_y_cd[i],0)
-            combinancion = factorial(n)/((factorial(i))*(factorial(n-i)))
-            cuenta = cuenta + combinancion*(Propabilidad**(n-i))*((uno_probabilidad)**i)*cu 
+            combinacion = factorial(n)/((factorial(i))*(factorial(n-i)))
+            cuenta = cuenta + combinacion*(Propabilidad**(n-i))*((uno_probabilidad)**i)*cu 
         valor_del_put = valor_presente1 * cuenta
         return valor_del_put, Nodos
 
     elif opcion == 3: #Call Americano
         Nodos = nodos(s,u,d,n)
-        lista_de_nodos = []
         lcu_y_cd = []
-        lista_llaves = []
-        for value in Nodos.values():
-            valor = value 
-            lista_de_nodos.append(valor)
-        for i in range(len(lista_de_nodos)-(n+1)):
-            lcu_y_cd.append(lista_de_nodos[i])
+        lcu_y_cd_nuevo = []
+
+        for i in range(len(Nodos)-(n+1)):
+            lcu_y_cd.append(Nodos[i])
         lcu_y_cd = lcu_y_cd[::-1]
-        for i in range(len(lcu_y_cd)-1):
-            nodo_evaluar = lcu_y_cd[i]
+        lcu_y_cd_nuevo = lcu_y_cd.copy()
+        for i in range(n): #Evaluamos los ultimos n valores desde el cero
+            nodo_evaluar = lcu_y_cd_nuevo[i]
             up = nodo_evaluar*u
             maxup = max(up-k,0)
-            down = nodo_evaluar*d
+            down = nodo_evaluar*d                 
             maxdown = max(down-k,0)
             call = valor_presenteT*(maxup*Propabilidad + maxdown*uno_probabilidad)
             rendimiento_ejercer = max(nodo_evaluar-k,0)
             if rendimiento_ejercer >= call:
-                lcu_y_cd[i] = rendimiento_ejercer
+                lcu_y_cd_nuevo[i] = rendimiento_ejercer
             else:
-                lcu_y_cd[i] = call
+                lcu_y_cd_nuevo[i] = call         #Hasta aquí solo han cambiado los penultimos 
+        #Vamos a comparar las listas lcy_y_lcd vs lcu_ylcd_nuevo para esto se necesitan en orden Ascendente
         lcu_y_cd = lcu_y_cd[::-1]
-        for value in Nodos.keys():
-            llave = value 
-            lista_llaves.append(llave)
-        for i in range(len(lcu_y_cd)):
-            Nodos[lista_llaves[i]] = lcu_y_cd[i]
-        valor_del_call_americano = valor_presenteT*(Propabilidad*lcu_y_cd[1] + uno_probabilidad*lcu_y_cd[2])
-        Nodos['A'] = valor_del_call_americano
+        lcu_y_cd_nuevo = lcu_y_cd_nuevo[::-1]
+        i=0
+        for j in lcu_y_cd_nuevo:
+            if j != lcu_y_cd[i]:
+                indice = lcu_y_cd_nuevo.index(j)
+                break
+            i+=1
+            
+        #Ahora empezamos a cambiar los elementos a partir de indice-1
+        for i in range(len(lcu_y_cd)-(n)): #A la longitud de la lista de nodos recortada le restamos los n valores desde el cero evaluados antes
+            nodo_evaluar =  lcu_y_cd_nuevo[indice-1]
+            call = valor_presenteT*(lcu_y_cd_nuevo[indice+(n-2)]*Propabilidad + lcu_y_cd_nuevo[indice+(n-1)]*uno_probabilidad)
+            rendimiento_ejercer = max(k-nodo_evaluar,0)
+            if rendimiento_ejercer >= call:
+                lcu_y_cd_nuevo[indice-1] = rendimiento_ejercer
+            else:
+                lcu_y_cd_nuevo[indice-1] = call
+            indice = indice -1 
+        lcu_y_cd_nuevo = lcu_y_cd_nuevo[::-1]
+        valor_del_call_americano = valor_presenteT*(Propabilidad*lcu_y_cd_nuevo[1] + uno_probabilidad*lcu_y_cd_nuevo[2])
         return valor_del_call_americano, Nodos
 
 
     elif opcion == 4:    #Put Americano
         Nodos = nodos(s,u,d,n)
-        lista_de_nodos = []
         lcu_y_cd = []
         lcu_y_cd_nuevo = []
-        lista_llaves = []
-        for value in Nodos.values():
-            valor = value 
-            lista_de_nodos.append(valor)
-        for i in range(len(lista_de_nodos)-(n+1)):
-            lcu_y_cd.append(lista_de_nodos[i])
+
+        for i in range(len(Nodos)-(n+1)):
+            lcu_y_cd.append(Nodos[i])
         lcu_y_cd = lcu_y_cd[::-1]
-        lcu_y_cd_nuevo = lcu_y_cd
+        lcu_y_cd_nuevo = lcu_y_cd.copy()
         for i in range(n): #Evaluamos los ultimos n valores desde el cero
             nodo_evaluar = lcu_y_cd_nuevo[i]
             up = nodo_evaluar*u
@@ -164,19 +159,31 @@ def mbinomial(s,opcion,T,n,r,k,u,d): #Funcion para determinar el precio de la op
                 lcu_y_cd_nuevo[i] = rendimiento_ejercer
             else:
                 lcu_y_cd_nuevo[i] = put         #Hasta aquí solo han cambiado los penultimos 
-        #Vamos a comparar las listas lcy_y_lcd vs lcu_ylcd_nuevo
+        #Vamos a comparar las listas lcy_y_lcd vs lcu_ylcd_nuevo para esto se necesitan en orden Ascendente
+        lcu_y_cd = lcu_y_cd[::-1]
+        lcu_y_cd_nuevo = lcu_y_cd_nuevo[::-1]
+        i=0
+        for j in lcu_y_cd_nuevo:
+            if j != lcu_y_cd[i]:
+                indice = lcu_y_cd_nuevo.index(j)
+                break
+            i+=1
+            
+        #Ahora empezamos a cambiar los elementos a partir de indice-1
         for i in range(len(lcu_y_cd)-(n)): #A la longitud de la lista de nodos recortada le restamos los n valores desde el cero evaluados antes
-            pass     
-        for value in Nodos.keys():        # Rehaciendo el diccionario de nodos
-            llave = value 
-            lista_llaves.append(llave)
-        for i in range(len(lcu_y_cd)):
-            Nodos[lista_llaves[i]] = lcu_y_cd[i]
-        valor_del_put_americano = valor_presenteT*(Propabilidad*lcu_y_cd[1] + uno_probabilidad*lcu_y_cd[2])
-        Nodos['A'] = valor_del_put_americano
+            nodo_evaluar =  lcu_y_cd_nuevo[indice-1]
+            put = valor_presenteT*(lcu_y_cd_nuevo[indice+(n-2)]*Propabilidad + lcu_y_cd_nuevo[indice+(n-1)]*uno_probabilidad)
+            rendimiento_ejercer = max(k-nodo_evaluar,0)
+            if rendimiento_ejercer >= put:
+                lcu_y_cd_nuevo[indice-1] = rendimiento_ejercer
+            else:
+                lcu_y_cd_nuevo[indice-1] = put
+            indice = indice -1 
+        lcu_y_cd_nuevo = lcu_y_cd_nuevo[::-1]
+        valor_del_put_americano = valor_presenteT*(Propabilidad*lcu_y_cd_nuevo[1] + uno_probabilidad*lcu_y_cd_nuevo[2])
         return valor_del_put_americano, Nodos
 
-def tabla_comparativa(s,T,n,t,k,u,d):
+def tabla_comparativa(s,T,n,r,k,u,d):
     datos={'Opción':['Europeo', 'Europeo','Americano', 'Americano'], 'Precio/Nodos':[mbinomial(s,1,T,n,r,k,u,d),
     mbinomial(s,2,T,n,r,k,u,d),mbinomial(s,3,T,n,r,k,u,d),mbinomial(s,4,T,n,r,k,u,d)]}
     tabla_datos = DataFrame(datos, columns = ['Opción','Precio/Nodos'], 
