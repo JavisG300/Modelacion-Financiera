47c2c550a78faed3c933763478317b3c7e3f2922
Javier Andrade <javieregonzaleza@gmail.com>
2022-05-26 16:02:23 -0500

    Primera prueba binomial con volatilidad

--- ModeloBinomial.py
+++ ModeloBinomial.py
@@ -26,11 +26,7 @@ def nodos(s,u,d,n):
         i+=1
     return lista_nodos
 
-def tabla(s,opcion,T,n,r,k,u,d):
-    T = T/12
-    Dt = T/n
-    Propabilidad = (np.exp(r*Dt) - d)/(u - d)
-    uno_probabilidad = 1 - Propabilidad
+def tabla(s,opcion,T,n,r,k,u,d,v,Dt,Probabilidad,uno_probabilidad):
     if opcion == 1:
         option = 'Call Europeo'
     elif opcion ==2:
@@ -39,19 +35,23 @@ def tabla(s,opcion,T,n,r,k,u,d):
         option = 'Call Americano'
     elif opcion == 4:
         option = 'Put Americano'
-    datos={'Dato         ':['Opción','S0','k','T','n','r','u','d','Dt','Probabilidad a la alza', 'Probabilidad a la baja'],
-    '   Valor ingresado   ':[option, s,k,T,n,r,u,d,Dt,Propabilidad,uno_probabilidad]}
-    tabla_datos = DataFrame(datos, columns = ['Dato         ','   Valor ingresado   '], 
-    index=['Tipo de opción','Precio incial del activo subyacente','Precio de ejercicio','Tiempo de vencimiento','Número de periodos',
-    'Tasa libre de riesgo','Porcentaje de subida','Porcentaje de bajada', 'Delta t', 'Probabilidad a la alza', 'Probabilidad a la baja'])
+    if v == 0:
+        datos={'Dato         ':['Opción','S0','k','T','n','r','u','d','Dt','Probabilidad a la alza', 'Probabilidad a la baja'],
+        '   Valor ingresado   ':[option, s,k,T,n,r,u,d,Dt,Probabilidad,uno_probabilidad]}
+        tabla_datos = DataFrame(datos, columns = ['Dato         ','   Valor ingresado   '], 
+        index=['Tipo de opción','Precio incial del activo subyacente','Precio de ejercicio','Tiempo de vencimiento','Número de periodos',
+        'Tasa libre de riesgo','Porcentaje de subida','Porcentaje de bajada', 'Delta t', 'Probabilidad a la alza', 'Probabilidad a la baja'])
+    else:
+        datos={'Dato         ':['Opción','S0','k','T','n','r','v','u','d','Dt','Probabilidad a la alza', 'Probabilidad a la baja'],
+        '   Valor ingresado   ':[option, s,k,T,n,r,v,u,d,Dt,Probabilidad,uno_probabilidad]}
+        tabla_datos = DataFrame(datos, columns = ['Dato         ','   Valor ingresado   '], 
+        index=['Tipo de opción','Precio incial del activo subyacente','Precio de ejercicio','Tiempo de vencimiento','Número de periodos',
+        'Tasa libre de riesgo','Volatilidad','Porcentaje de subida','Porcentaje de bajada', 'Delta t', 'Probabilidad a la alza', 'Probabilidad a la baja'])
     print('\n')
     print(tabla_datos.round(4))
 
-def mbinomial(s,T,n,r,k,u,d): #Funcion para determinar el precio de las opcion
-    T = T/12
-    Dt = T/n
-    Propabilidad = (np.exp(r*Dt) - d)/(u - d)
-    uno_probabilidad = 1 - Propabilidad
+def mbinomial(s,n,r,k,u,d,Dt,Probabilidad,uno_probabilidad): #Funcion para determinar el precio de las opcion
+    
     valor_presente1 = np.exp(-n*r*Dt)
     valor_presenteT = np.exp(-r*Dt)
 
@@ -73,8 +73,8 @@ def mbinomial(s,T,n,r,k,u,d): #Funcion para determinar el precio de las opcion
         #Put Europeo
         pu = max(k-lcu_y_cd[i],0)
         combinacion = factorial(n)/((factorial(i))*(factorial(n-i)))
-        cuenta_call = cuenta_call + combinacion*(Propabilidad**(n-i))*((uno_probabilidad)**i)*cu
-        cuenta_put = cuenta_put + combinacion*(Propabilidad**(n-i))*((uno_probabilidad)**i)*pu 
+        cuenta_call = cuenta_call + combinacion*(Probabilidad**(n-i))*((uno_probabilidad)**i)*cu
+        cuenta_put = cuenta_put + combinacion*(Probabilidad**(n-i))*((uno_probabilidad)**i)*pu 
     valor_del_call = valor_presente1 * cuenta_call
     valor_del_put = valor_presente1 * cuenta_put
 
@@ -100,8 +100,8 @@ def mbinomial(s,T,n,r,k,u,d): #Funcion para determinar el precio de las opcion
         maxdown_call = max(down-k,0)
         maxdown_put = max(k-down,0)
 
-        call = valor_presenteT*(maxup_call*Propabilidad + maxdown_call*uno_probabilidad)
-        put = valor_presenteT*(maxup_put*Propabilidad + maxdown_put*uno_probabilidad)
+        call = valor_presenteT*(maxup_call*Probabilidad + maxdown_call*uno_probabilidad)
+        put = valor_presenteT*(maxup_put*Probabilidad + maxdown_put*uno_probabilidad)
         rendimiento_ejercer_call = max(nodo_evaluar-k,0)
         rendimiento_ejercer_put = max(k-nodo_evaluar,0)
 
@@ -136,8 +136,8 @@ def mbinomial(s,T,n,r,k,u,d): #Funcion para determinar el precio de las opcion
     for i in range(len(lcu_y_cd)-(n)): #A la longitud de la lista de nodos recortada le restamos los n valores desde el cero evaluados antes
         nodo_evaluar_call =  lcu_y_cd_call[indice_call-1]
         nodo_evaluar_put =  lcu_y_cd_put[indice_put-1]
-        call = valor_presenteT*(lcu_y_cd_call[indice_call+(N-2)]*Propabilidad + lcu_y_cd_call[indice_call+(N-1)]*uno_probabilidad)
-        put = valor_presenteT*(lcu_y_cd_put[indice_put+(N-2)]*Propabilidad + lcu_y_cd_put[indice_put+(N-1)]*uno_probabilidad)
+        call = valor_presenteT*(lcu_y_cd_call[indice_call+(N-2)]*Probabilidad + lcu_y_cd_call[indice_call+(N-1)]*uno_probabilidad)
+        put = valor_presenteT*(lcu_y_cd_put[indice_put+(N-2)]*Probabilidad + lcu_y_cd_put[indice_put+(N-1)]*uno_probabilidad)
         rendimiento_ejercer_call = max(nodo_evaluar_call-k,0)
         rendimiento_ejercer_put = max(k-nodo_evaluar_put,0)
         if rendimiento_ejercer_call >= call:
@@ -155,13 +155,13 @@ def mbinomial(s,T,n,r,k,u,d): #Funcion para determinar el precio de las opcion
         if contador == N-1:
             N = N-1
             contador = 0
-    valor_del_call_americano = valor_presenteT*(Propabilidad*lcu_y_cd_call[1] + uno_probabilidad*lcu_y_cd_call[2])
-    valor_del_put_americano = valor_presenteT*(Propabilidad*lcu_y_cd_put[1] + uno_probabilidad*lcu_y_cd_put[2])
+    valor_del_call_americano = valor_presenteT*(Probabilidad*lcu_y_cd_call[1] + uno_probabilidad*lcu_y_cd_call[2])
+    valor_del_put_americano = valor_presenteT*(Probabilidad*lcu_y_cd_put[1] + uno_probabilidad*lcu_y_cd_put[2])
 
     return valor_del_call, valor_del_put, valor_del_call_americano, valor_del_put_americano
 
-def tabla_comparativa(s,T,n,r,k,u,d):
-    uno = mbinomial(s,T,n,r,k,u,d)
+def tabla_comparativa():
+    uno = mbinomial(s,n,r,k,u,d,Dt,Probabilidad,uno_probabilidad)
     datos={'Opción':['Europeo', 'Europeo','Americano', 'Americano'], 'Precio':[uno[0],uno[1],uno[2],uno[3]]}
     tabla_datos = DataFrame(datos, columns = ['Opción','Precio'], 
     index=['Call','Put','Call','Put'])
@@ -184,35 +184,44 @@ Escribe el número de la opción que será valuada
 4) Put Americano 
  """)) 
 modelo = input("""
-¿Qué modelo quieres utilizar para valuar la opción?
+¿Qué modelo quieres utilizar para valuar la opción? 
 A) Binomial
 B) Binomial con volatilidad
 C) Black & Scholes - Merton
 """)
+modelo = modelo.upper()
 print(f'---Su selección fue el inciso {opcion} ---\n')
 s      = float(input("Indica el precio incial del activo subyacente: "))
 k      = float(input("Indica el precio de ejercicio: "))
 T      = float(input("Indica el tiempo de vencimiento en meses: "))
-n      = int(input("Indica el número de periodos: "))
+T = T/12
 r      = float(input("Indica la tasa de interés anualizada de forma decimal (Ej. 0.05): "))
 if modelo == 'A':
+    n  = int(input("Indica el número de periodos: "))
+    Dt = T/n
     u  = float(input("Indica el porcentaje/probabilidad de subida con la unidad (Ej. 1.05): "))
     d  = float(input("Indica el porcentaje/probabilidad de bajada con la unidad (Ej. 0.95): "))
+    v  = 0
 elif modelo == 'B':
+    n  = int(input("Indica el número de periodos: "))
+    Dt = T/n
     v  = float(input("Indica la volatilidad de manera decimal (Ej. 0.25)"))
+    u  = np.exp(v*np.sqrt(Dt))
+    d  = np.exp(-v*np.sqrt(Dt))
 elif modelo == 'C':
     pass
-u      = float(input("Indica el porcentaje/probabilidad de subida con la unidad: "))
-d      = float(input("Indica el porcentaje/probabilidad de bajada con la unidad: "))
+
+Probabilidad = (np.exp(r*Dt) - d)/(u - d)
+uno_probabilidad = 1 - Probabilidad
 
 def main():
     print("""
     ---------------------------------------
     || Resumen de los datos introducidos ||
     ---------------------------------------""")
-    tabla(s,opcion,T,n,r,k,u,d)
+    tabla(s,opcion,T,n,r,k,u,d,v,Dt,Probabilidad,uno_probabilidad)
     nodos(s,u,d,n)
-    l = mbinomial(s,T,n,r,k,u,d)
+    l = mbinomial(s,n,r,k,u,d,Dt,Probabilidad,uno_probabilidad)
     if opcion == 1:
         tipo = 'Call Europeo'
         v = 0
@@ -232,7 +241,7 @@ def main():
     || A continuación se muestra una tabla comparativa de la opción solicitada junto a los demás tipos de opción || 
     ---------------------------------------------------------------------------------------------------------------
     """)
-    tabla_comparativa(s,T,n,r,k,u,d)
+    tabla_comparativa()
     print('\n')
     Paridad = input(('¿Quieres comprobar la paridad Put-Call entre algunas de las opciones. y/n: '))
     Paridad = Paridad.lower()
@@ -265,10 +274,10 @@ def main():
             
         t=T/12
         paridad(c,p,k,r,s,t)
-        print('\n https://github.com/JavisG300/Modelacion-Financiera/blob/master/ModeloBinomial.py')
+        print('\n Si deseas colaborar https://github.com/JavisG300/Modelacion-Financiera/blob/master/ModeloBinomial.py')
     else:
         print('El programa finalizó')
-        print('\n https://github.com/JavisG300/Modelacion-Financiera/blob/master/ModeloBinomial.py')
+        print('\n Si deseas colaborar https://github.com/JavisG300/Modelacion-Financiera/blob/master/ModeloBinomial.py')
 
 
 if __name__ == '__main__':
