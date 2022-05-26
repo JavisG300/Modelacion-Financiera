05d0344fe41faef21ba7bfaa8d6b4874a4589d40
Javier Andrade <javieregonzaleza@gmail.com>
2022-05-25 23:45:08 -0500

    Antes de super recortar

--- ModeloBinomial.py
+++ ModeloBinomial.py
@@ -47,7 +47,7 @@ def tabla(s,opcion,T,n,r,k,u,d):
     print('\n')
     print(tabla_datos.round(4))
 
-def mbinomial(s,opcion,T,n,r,k,u,d): #Funcion para determinar el precio de las opcion
+def mbinomial(s,T,n,r,k,u,d): #Funcion para determinar el precio de las opcion
     T = T/12
     Dt = T/n
     Propabilidad = (np.exp(r*Dt) - d)/(u - d)
@@ -56,137 +56,137 @@ def mbinomial(s,opcion,T,n,r,k,u,d): #Funcion para determinar el precio de las o
     valor_presenteT = np.exp(-r*Dt)
 
     #Calculando el valor de la opción
-    if opcion == 1:   #Call Europeo
-        cuenta = 0
-        Nodos = nodos(s,u,d,n)
-        Nodos1 = Nodos.copy()
-        Nodos1 = Nodos1[::-1]
-        lcu_y_cd = []
-        for i in range(n+1):
-            lcu_y_cd.append(Nodos1[i])
-        lcu_y_cd = lcu_y_cd[::-1]
-        for i in range(n+1):
-            cu = max(lcu_y_cd[i]-k,0)
-            combinacion = factorial(n)/((factorial(i))*(factorial(n-i)))
-            cuenta = cuenta + combinacion*(Propabilidad**(n-i))*((uno_probabilidad)**i)*cu 
-        valor_del_call = valor_presente1 * cuenta
+    #Call Europeo
+    cuenta = 0
+    Nodos = nodos(s,u,d,n)
+    Nodos1 = Nodos.copy()
+    Nodos1 = Nodos1[::-1]
+    lcu_y_cd = []
+    for i in range(n+1):
+        lcu_y_cd.append(Nodos1[i])
+    lcu_y_cd = lcu_y_cd[::-1]
+    for i in range(n+1):
+        cu = max(lcu_y_cd[i]-k,0)
+        combinacion = factorial(n)/((factorial(i))*(factorial(n-i)))
+        cuenta = cuenta + combinacion*(Propabilidad**(n-i))*((uno_probabilidad)**i)*cu 
+    valor_del_call = valor_presente1 * cuenta
     
 
-    elif opcion == 2:  #Put Europeo
-        cuenta = 0
-        Nodos = nodos(s,u,d,n)
-        Nodos1 = Nodos.copy()
-        Nodos1 = Nodos1[::-1]
-        lcu_y_cd = []     
-        for i in range(n+1):
-            lcu_y_cd.append(Nodos1[i])
-        lcu_y_cd = lcu_y_cd[::-1]
-        for i in range(n+1):
-            cu = max(k-lcu_y_cd[i],0)
-            combinacion = factorial(n)/((factorial(i))*(factorial(n-i)))
-            cuenta = cuenta + combinacion*(Propabilidad**(n-i))*((uno_probabilidad)**i)*cu 
-        valor_del_put = valor_presente1 * cuenta
+    #Put Europeo
+    cuenta = 0
+    Nodos = nodos(s,u,d,n)
+    Nodos1 = Nodos.copy()
+    Nodos1 = Nodos1[::-1]
+    lcu_y_cd = []     
+    for i in range(n+1):
+        lcu_y_cd.append(Nodos1[i])
+    lcu_y_cd = lcu_y_cd[::-1]
+    for i in range(n+1):
+        cu = max(k-lcu_y_cd[i],0)
+        combinacion = factorial(n)/((factorial(i))*(factorial(n-i)))
+        cuenta = cuenta + combinacion*(Propabilidad**(n-i))*((uno_probabilidad)**i)*cu 
+    valor_del_put = valor_presente1 * cuenta
         
 
-    elif opcion == 3: #Call Americano
-        Nodos = nodos(s,u,d,n) #Lista de los nodos de subida y bajada
-        lcu_y_cd = []
-        lcu_y_cd_nuevo = []
+    #Call Americano
+    Nodos = nodos(s,u,d,n) #Lista de los nodos de subida y bajada
+    lcu_y_cd = []
+    lcu_y_cd_nuevo = []
 
-        for i in range(len(Nodos)-(n+1)):
-            lcu_y_cd.append(Nodos[i])
-        lcu_y_cd = lcu_y_cd[::-1]
-        lcu_y_cd_nuevo = lcu_y_cd.copy()
+    for i in range(len(Nodos)-(n+1)):
+        lcu_y_cd.append(Nodos[i])
+    lcu_y_cd = lcu_y_cd[::-1]
+    lcu_y_cd_nuevo = lcu_y_cd.copy()
 
-        for i in range(n): #Evaluamos los ultimos n valores desde el cero es decir n+1
-            nodo_evaluar = lcu_y_cd_nuevo[i]
-            up = nodo_evaluar*u
-            maxup = max(up-k,0)
-            down = nodo_evaluar*d                 
-            maxdown = max(down-k,0)
-            call = valor_presenteT*(maxup*Propabilidad + maxdown*uno_probabilidad)
-            rendimiento_ejercer = max(nodo_evaluar-k,0)
-            if rendimiento_ejercer >= call:
-                lcu_y_cd_nuevo[i] = rendimiento_ejercer
-            else:
-                lcu_y_cd_nuevo[i] = call         #Hasta aquí solo han cambiado los penultimos 
+    for i in range(n): #Evaluamos los ultimos n valores desde el cero es decir n+1
+        nodo_evaluar = lcu_y_cd_nuevo[i]
+        up = nodo_evaluar*u
+        maxup = max(up-k,0)
+        down = nodo_evaluar*d                 
+        maxdown = max(down-k,0)
+        call = valor_presenteT*(maxup*Propabilidad + maxdown*uno_probabilidad)
+        rendimiento_ejercer = max(nodo_evaluar-k,0)
+        if rendimiento_ejercer >= call:
+            lcu_y_cd_nuevo[i] = rendimiento_ejercer
+        else:
+            lcu_y_cd_nuevo[i] = call         #Hasta aquí solo han cambiado los penultimos 
         #Vamos a comparar las listas lcy_y_lcd vs lcu_ylcd_nuevo para esto se necesitan en orden Ascendente
-        lcu_y_cd = lcu_y_cd[::-1]
-        lcu_y_cd_nuevo = lcu_y_cd_nuevo[::-1]
-        i=0
-        for j in lcu_y_cd_nuevo:
-            if j != lcu_y_cd[i]:
-                indice = lcu_y_cd_nuevo.index(j)
-                break
-            i+=1
-        contador = 0
-        N=n
+    lcu_y_cd = lcu_y_cd[::-1]
+    lcu_y_cd_nuevo = lcu_y_cd_nuevo[::-1]
+    i=0
+    for j in lcu_y_cd_nuevo:
+        if j != lcu_y_cd[i]:
+            indice = lcu_y_cd_nuevo.index(j)
+            break
+        i+=1
+    contador = 0
+    N=n
         #Ahora empezamos a cambiar los elementos a partir de indice-1
-        for i in range(len(lcu_y_cd)-(n)): #A la longitud de la lista de nodos recortada le restamos los n valores desde el cero evaluados antes
-            nodo_evaluar =  lcu_y_cd_nuevo[indice-1]
-            call = valor_presenteT*(lcu_y_cd_nuevo[indice+(N-2)]*Propabilidad + lcu_y_cd_nuevo[indice+(N-1)]*uno_probabilidad)
-            rendimiento_ejercer = max(nodo_evaluar-k,0)
-            if rendimiento_ejercer >= call:
-                lcu_y_cd_nuevo[indice-1] = rendimiento_ejercer
-            else:
-                lcu_y_cd_nuevo[indice-1] = call
-            indice = indice -1 
-            contador = contador + 1
-            if contador == N-1:
-                N = N-1
-                contador = 0
-        valor_del_call_americano = valor_presenteT*(Propabilidad*lcu_y_cd_nuevo[1] + uno_probabilidad*lcu_y_cd_nuevo[2])
+    for i in range(len(lcu_y_cd)-(n)): #A la longitud de la lista de nodos recortada le restamos los n valores desde el cero evaluados antes
+        nodo_evaluar =  lcu_y_cd_nuevo[indice-1]
+        call = valor_presenteT*(lcu_y_cd_nuevo[indice+(N-2)]*Propabilidad + lcu_y_cd_nuevo[indice+(N-1)]*uno_probabilidad)
+        rendimiento_ejercer = max(nodo_evaluar-k,0)
+        if rendimiento_ejercer >= call:
+            lcu_y_cd_nuevo[indice-1] = rendimiento_ejercer
+        else:
+            lcu_y_cd_nuevo[indice-1] = call
+        indice = indice -1 
+        contador = contador + 1
+        if contador == N-1:
+            N = N-1
+            contador = 0
+    valor_del_call_americano = valor_presenteT*(Propabilidad*lcu_y_cd_nuevo[1] + uno_probabilidad*lcu_y_cd_nuevo[2])
         
 
 
-    elif opcion == 4:    #Put Americano
-        Nodos = nodos(s,u,d,n)
-        lcu_y_cd = []
-        lcu_y_cd_nuevo = []
+    #Put Americano
+    Nodos = nodos(s,u,d,n)
+    lcu_y_cd = []
+    lcu_y_cd_nuevo = []
 
-        for i in range(len(Nodos)-(n+1)):
-            lcu_y_cd.append(Nodos[i])
-        lcu_y_cd = lcu_y_cd[::-1]
-        lcu_y_cd_nuevo = lcu_y_cd.copy()
-        for i in range(n): #Evaluamos los ultimos n valores desde el cero
-            nodo_evaluar = lcu_y_cd_nuevo[i]
-            up = nodo_evaluar*u
-            maxup = max(k-up,0)
-            down = nodo_evaluar*d                 
-            maxdown = max(k-down,0)
-            put = valor_presenteT*(maxup*Propabilidad + maxdown*uno_probabilidad)
-            rendimiento_ejercer = max(k-nodo_evaluar,0)
-            if rendimiento_ejercer >= put:
-                lcu_y_cd_nuevo[i] = rendimiento_ejercer
-            else:
-                lcu_y_cd_nuevo[i] = put         #Hasta aquí solo han cambiado los penultimos 
+    for i in range(len(Nodos)-(n+1)):
+        lcu_y_cd.append(Nodos[i])
+    lcu_y_cd = lcu_y_cd[::-1]
+    lcu_y_cd_nuevo = lcu_y_cd.copy()
+    for i in range(n): #Evaluamos los ultimos n valores desde el cero
+        nodo_evaluar = lcu_y_cd_nuevo[i]
+        up = nodo_evaluar*u
+        maxup = max(k-up,0)
+        down = nodo_evaluar*d                 
+        maxdown = max(k-down,0)
+        put = valor_presenteT*(maxup*Propabilidad + maxdown*uno_probabilidad)
+        rendimiento_ejercer = max(k-nodo_evaluar,0)
+        if rendimiento_ejercer >= put:
+            lcu_y_cd_nuevo[i] = rendimiento_ejercer
+        else:
+            lcu_y_cd_nuevo[i] = put         #Hasta aquí solo han cambiado los penultimos 
         #Vamos a comparar las listas lcy_y_lcd vs lcu_ylcd_nuevo para esto se necesitan en orden Ascendente
-        lcu_y_cd = lcu_y_cd[::-1]
-        lcu_y_cd_nuevo = lcu_y_cd_nuevo[::-1]
-        i=0
-        for j in lcu_y_cd_nuevo:
-            if j != lcu_y_cd[i]:
-                indice = lcu_y_cd_nuevo.index(j)
-                break
-            i+=1
-        contador=0 
-        N=n   
+    lcu_y_cd = lcu_y_cd[::-1]
+    lcu_y_cd_nuevo = lcu_y_cd_nuevo[::-1]
+    i=0
+    for j in lcu_y_cd_nuevo:
+        if j != lcu_y_cd[i]:
+            indice = lcu_y_cd_nuevo.index(j)
+            break
+        i+=1
+    contador=0 
+    N=n   
         #Ahora empezamos a cambiar los elementos a partir de indice-1
-        for i in range(len(lcu_y_cd)-(n)): #A la longitud de la lista de nodos recortada le restamos los n valores desde el cero evaluados antes
-            nodo_evaluar =  lcu_y_cd_nuevo[indice-1]
-            put = valor_presenteT*(lcu_y_cd_nuevo[indice+(N-2)]*Propabilidad + lcu_y_cd_nuevo[indice+(N-1)]*uno_probabilidad)
-            rendimiento_ejercer = max(k-nodo_evaluar,0)
-            if rendimiento_ejercer >= put:
-                lcu_y_cd_nuevo[indice-1] = rendimiento_ejercer
-            else:
-                lcu_y_cd_nuevo[indice-1] = put
-            indice = indice -1 
-            contador = contador + 1
-            if contador == N-1:
-                N = N-1
-                contador = 0
+    for i in range(len(lcu_y_cd)-(n)): #A la longitud de la lista de nodos recortada le restamos los n valores desde el cero evaluados antes
+        nodo_evaluar =  lcu_y_cd_nuevo[indice-1]
+        put = valor_presenteT*(lcu_y_cd_nuevo[indice+(N-2)]*Propabilidad + lcu_y_cd_nuevo[indice+(N-1)]*uno_probabilidad)
+        rendimiento_ejercer = max(k-nodo_evaluar,0)
+        if rendimiento_ejercer >= put:
+            lcu_y_cd_nuevo[indice-1] = rendimiento_ejercer
+        else:
+            lcu_y_cd_nuevo[indice-1] = put
+        indice = indice -1 
+        contador = contador + 1
+        if contador == N-1:
+            N = N-1
+            contador = 0
 
-        valor_del_put_americano = valor_presenteT*(Propabilidad*lcu_y_cd_nuevo[1] + uno_probabilidad*lcu_y_cd_nuevo[2])
+    valor_del_put_americano = valor_presenteT*(Propabilidad*lcu_y_cd_nuevo[1] + uno_probabilidad*lcu_y_cd_nuevo[2])
     return valor_del_call, valor_del_put, valor_del_call_americano, valor_del_put_americano
 
 def tabla_comparativa(s,T,n,r,k,u,d):
